e135becacb4e72ddadbee767dcdadc03528bc3d7
Chinfal <chenmr.cyf@qq.com>
2024-03-15 10:37:25 +0800

    feat: project's file init .

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [vue()],
+})
